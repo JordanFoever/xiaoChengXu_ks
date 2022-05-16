8485b8b8666bd74faa6060f4bd3a6ef6f6078212
卫江权 <1227105586@qq.com>
2022-05-16 19:53:55 +0800

    完成首页正在热映的静态界面

---
+++ pages/home/home.js
@@ -0,0 +1,37 @@
+// pages/home/home.js
+Page({
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        navList:["正在热映","即将上映"],
+		select:[true,false],
+		imageList:[
+			"../../image/1.jpg",
+			"../../image/2.jpg",
+			"../../image/3.jpg",
+			"../../image/4.jpg"
+		]
+    },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad(options) {
+
+    },
+    // 切换导航栏时(获取点击时自定义的样式)
+    navClick(e){
+        let index = e.currentTarget.dataset.index;
+        if(index === 0){
+            this.setData({
+                select:[true,false]
+            })
+        }else{
+            this.setData({
+                select:[false,true]
+            })
+        }
+        console.log(index);
+    }
+    
+})
